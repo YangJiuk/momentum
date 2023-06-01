f7b056adea28fea34918716b7b679cb16049c4ec
YangJiuk <106547176+YangJiuk@users.noreply.github.com>
2023-06-02 01:13:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-var popup = document.getElementById('popup');
-var popupImg = document.getElementById('popup-img');
-var codeBlock = document.querySelector('pre');
 
-codeBlock.addEventListener('mouseover', function() {
-    popupImg.src = 'Diablo-IV.png'; 
-    popup.classList.remove('hidden');
-});
-
-codeBlock.addEventListener('mouseout', function() {
-    popup.classList.add('hidden');
-});
