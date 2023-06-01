190cddd0a66ff58d700c863f3288362fe6d6360d
YangJiuk <106547176+YangJiuk@users.noreply.github.com>
2023-06-02 01:07:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.getElementById("content").textContent = /* 3000자 이상의 내용 */;
+var popup = document.getElementById('popup');
+var popupImg = document.getElementById('popup-img');
+var codeBlock = document.querySelector('pre');
+
+codeBlock.addEventListener('mouseover', function() {
+    popupImg.src = 'image.jpg';  // 이미지 URL을 여기에 넣으세요.
+    popup.classList.remove('hidden');
+});
+
+codeBlock.addEventListener('mouseout', function() {
+    popup.classList.add('hidden');
+});
